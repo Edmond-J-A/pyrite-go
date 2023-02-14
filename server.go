a98318e050ec47ff64966b16d3b8ceb13f87adbe
Edmond-J-A <edomondja@gmail.com>
2023-02-14 17:06:20 +0800

    impl: SetSession

--- server.go
+++ server.go
@@ -35,14 +35,8 @@ func (s *Server) SetSession(session string, data interface{}) {
 	s.session[session] = data
 }
 
-func (s *Server) DelSession(session string) {
-
-}
-
-func (s *Server) Tell(remote *net.UDPAddr, identifier, body string) (Response, error) {
-
-}
-
+func (s *Server) DelSession(session string)
+func (s *Server) Tell(remote *net.UDPAddr, identifier, body string) (Response, error)
 func (s *Server) Start() error {
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: s.port})
 	if err != nil {
